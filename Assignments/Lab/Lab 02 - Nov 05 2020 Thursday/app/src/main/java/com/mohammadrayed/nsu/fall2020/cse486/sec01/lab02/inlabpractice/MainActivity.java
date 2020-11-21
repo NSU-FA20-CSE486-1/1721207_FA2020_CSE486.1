c2eec8493170414f87bffe1b00e6d58af5778828
Mohammad Rayed <32837586+rayedsikder@users.noreply.github.com>
2020-11-21 20:32:47 +0600

    Enhancement #3 Fix toast message issue, improve the UI design
    1. Generated toast for both successful and unsuccessful login.
    2. Improved UI- reduced TextViews.
    3. Username & password fields indicated to user by using Hint attribute.
    
    However, couldn't fix the image issue

--- Assignments/Lab/Lab 02 - Nov 05 2020 Thursday/app/src/main/java/com/mohammadrayed/nsu/fall2020/cse486/sec01/lab02/inlabpractice/MainActivity.java
+++ Assignments/Lab/Lab 02 - Nov 05 2020 Thursday/app/src/main/java/com/mohammadrayed/nsu/fall2020/cse486/sec01/lab02/inlabpractice/MainActivity.java
@@ -4,15 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username=(EditText) findViewById(R.id.editTextTextEmailAddress);
+        password=(EditText) findViewById(R.id.editTextTextPassword);
+
     }
 
-   // public void show_attempt(View view) {
-   // }
+
+    public void showToast(View view) {
+        Toast toast_success=Toast.makeText(this, R.string.toast_msg_success, Toast.LENGTH_SHORT );
+        Toast toast_unsuccess=Toast.makeText(this, R.string.toast_msg_unsuccess, Toast.LENGTH_SHORT);
+        if(username.getText().toString().equals("lab02@gmail.com") && password.getText().toString().equals("password")) {
+            toast_success.show();
+        }
+        else{
+            toast_unsuccess.show();
+        }
+    }
 }
