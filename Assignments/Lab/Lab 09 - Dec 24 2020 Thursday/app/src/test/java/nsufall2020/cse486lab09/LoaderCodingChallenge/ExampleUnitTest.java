549e1ca26e741c59dfdb34fd3688484c254e93b5
Mohammad Rayed <32837586+rayedsikder@users.noreply.github.com>
2021-01-22 20:30:04 +0600

    Lab09 Assignment #36
    Solved the book search coding challenge

---
+++ Assignments/Lab/Lab 09 - Dec 24 2020 Thursday/app/src/test/java/nsufall2020/cse486lab09/LoaderCodingChallenge/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package nsufall2020.cse486lab09.LoaderCodingChallenge;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
